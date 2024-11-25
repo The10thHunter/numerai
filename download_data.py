49bfd36ea3e4daf4ca539ab2ed6d895d8d1e64d5
The10thHunter <mehtaprashant@icloud.com>
2024-11-25 13:56:03 -0800

    Modified download_data to support mySQL

--- download_data.py
+++ download_data.py
@@ -1,6 +1,13 @@
 #Download Script for NumerAI
 from numerapi import NumerAPI
+#Only import create_engine, pandas will handle everything else
+from sqlalchemy import create_engine 
 import pandas as pd 
 
 napi = NumerAPI()
-napi.download_dataset("v5.0/train.parquet")
+#Dataset already installed on local. Edit this out during commit 
+#napi.download_dataset("v5.0/train.parquet")
+engine = create_engine("mysql+pymysql://user:password@host:port/dbname")
+sql = pd.read_parquet("../v5.0/train.parquet")
+#"data" corresponds to table_name
+sql.to_sql("data", engine,index = True, index_col = "id")
--- model.py
+++ model.py
@@ -1,20 +1,31 @@
 import pandas as pd
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.model_selection import train_test_split
+#from sklearn.tree import DecisionTreeRegressor
+#from sklearn.model_selection import train_test_split
 """
 from sklearn import 
 from sklearn import 
 #...
 """
-#SEED = 
-data = pd.read_parquet("../v5.0/train.parquet")
+SEED = 0
+#data = pd.read_parquet("../v5.0/train.parquet").sample(frac = 0.1, random_state = SEED, replace = False)
 
-train = data[[a for a in data.columns if "feature" in a]]
-test = data[[b for b in data.columns if "target" in b]]
-data = None
-corr_matrix = train.corr(method = "pearson") #Method can be defined using a function.
 
-corr_matrix.to_csv()
+def corrTable(dataframe):
+    #test = dataframe[[b for b in dataframe.columns if "target" in b]]
+    corr_matrix = dataframe.corr(method = "pearson").abs()
+    corr_matrix = corr_matrix.unstack().reset_index()
+
+    corr_matrix.columns = ["feature1", "feature2", "correlation"]
+    corr_matrix = corr_matrix[corr_matrix["feature1"] != corr_matrix["feature2"]]
+    
+    return corr_matrix
+
+data = pd.read_parquet("../v5.0/train.parquet").sample(frac = 0.05, random_state = SEED)
+
+data = data.iloc[:,:50]
+print("Data Read.")
+final = corrTable(data)
+final.to_csv("sample_corr.csv")
 
 #print("Length of Training: " + str(len(train.columns)))
 #print("Length of Test: " + str(len(test.columns)))
