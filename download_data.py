e6cb8995a636579ff68a4a91293eb80eeb752cfd
The10thHunter <mehtaprashant@icloud.com>
2024-11-27 15:01:17 -0800

    download_data changes, converted for redundancy reasons

--- download_data.py
+++ download_data.py
@@ -1,13 +1,29 @@
 #Download Script for NumerAI
 from numerapi import NumerAPI
+from os import listdir
 #Only import create_engine, pandas will handle everything else
 from sqlalchemy import create_engine 
 import pandas as pd 
 
 napi = NumerAPI()
-#Dataset already installed on local. Edit this out during commit 
-#napi.download_dataset("v5.0/train.parquet")
 engine = create_engine("mysql+pymysql://user:password@host:port/dbname")
-sql = pd.read_parquet("../v5.0/train.parquet")
-#"data" corresponds to table_name
-sql.to_sql("data", engine,index = True, index_col = "id")
+#Dataset already installed on local. Edit this out during commit 
+
+def basicInstall(): 
+    if input("Install all? (y/n)").lower() == "y":
+        napi.download_dataset("v5.0/train.parquet")
+    sql = pd.read_parquet("/v5.0/train.parquet")
+    #train is the table name 
+    sql.to_sql("train", engine,index = True, index_col = "id")
+
+def allInstall():
+#If it is desirable, you may install all data formats this way instead: :  
+    from numerapi import NumerAPI
+    for file in [a for a in napi.list_datasets() if a.endswith(".parquet")]: 
+        napi.download_dataset(file)
+
+def autoSql(filepath, eng):
+#If you manually installed and just want an easy mySQL db conversion: 
+    for item in [a for a in listdir(filepath) if a.endswith(".parquet")]:
+        sql = pd.read_parquet(item)
+        sql.to_sql(item, eng, index = True, index_col = "id")
