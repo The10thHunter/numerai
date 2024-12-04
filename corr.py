c3ce00cf9d52f4d064c034fb5d109c97745f6eef
The10thHunter <mehtaprashant@icloud.com>
2024-12-04 13:38:16 -0800

    corr.py has time complexity texts

--- corr.py
+++ corr.py
@@ -1,4 +1,34 @@
 import pandas as pd 
-import numpy as np 
+import time 
+def test1(x):    
+    start = time.time()
+    data = pd.read_parquet("../v5.0/train.parquet")
+    data = data[[a for a in data.columns if "feature" in a]]
+    data = data.iloc[:,:x]
+    data.corr().to_csv("sample_corr.csv")
+    end = time.time()
+    return {x: x, y :end-start}
 
+def test2(x): 
+    start = time.time()
+    data = pd.read_parquet("../v5.0/train.parquet")
+    data = data[[a for a in data.columns if "feature" in a]]
 
+    #result = pd.DataFrame()
+    final = []
+    for z in range(0,x+1,1): 
+        colpop = data.columns[z]
+        curr_feature = data.pop(colpop)
+        for y in range(z, x+1, 1): 
+            corr_value = curr_feature.corr(data.iloc[y])
+            result = {"Feature 1" : colpop,
+                      "Feature 2" : data.iloc[y],
+                      "Corr" : corr_value}
+            final.append(result)
+
+    temp = pd.DataFrame(final).to_csv("sample_corr.csv")
+    end = time.time()
+    print(str(x) + " features in " + str(end-start) + " time (sec)")
+
+for x in range(50, 350, 50):
+    test2(x)
