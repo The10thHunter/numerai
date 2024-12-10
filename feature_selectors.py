e9d28f7ae3df1e9a7c32668b663335d267043194
The10thHunter <mehtaprashant@icloud.com>
2024-12-09 17:34:38 -0800

    Changes to feature_selector correcting for corr.py function errors

--- feature_selectors.py
+++ feature_selectors.py
@@ -1,5 +1,13 @@
 import pandas as pd 
+import numpy as np 
+data = pd.read_parquet("../v5.0/train.parquet")
+#len(data_features) = 2376
+data = data[[a for a in data.columns if "feature" in a]]
 
-corr = pd.read_csv("sample_corr.csv")
-corr = corr.drop_duplicates()
-print(corr.head())
+feats = np.array_split(data.columns, 24)
+# DEBUG: print(len(feats[1]), len(feats[0]), len(feats[23]))
+
+for chunk in feats: 
+    subtable = data[chunk].corr().melt(id_vars = data.columns, value_name = 'Correlation')
+
+    
