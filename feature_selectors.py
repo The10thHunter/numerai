90877614934aca932d9f75fcdec495b9119d16e0
The10thHunter <mehtaprashant@icloud.com>
2024-12-09 18:16:26 -0800

    Proto filter syntax issue with pandas

--- feature_selectors.py
+++ feature_selectors.py
@@ -6,12 +6,9 @@ data = data[[a for a in data.columns if "feature" in a]]
 
 feats = np.array_split(data.columns, 24)
 # DEBUG: print(len(feats[1]), len(feats[0]), len(feats[23]))
-features_useful = data.columns.tolist()
 for chunk in feats: 
     subtable = abs(data[chunk].corr())
     subtable = subtable.reset_index().melt(id_vars = "index", var_name = "feature2",value_name = 'Correlation')
     subtable.sort_values(by = "Correlation", ascending = False)
-    if subtable["Correlation"] < 0.90: 
-        features_useful.append(subtable["index"])
-
+    features_useful = subtable[(subtable["Correlation"] < 0.90)]["index"].tolist()
     print(len(features_useful))
