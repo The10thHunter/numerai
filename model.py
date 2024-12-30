2f24a7108dca3da76bdf1d466b41f29c0cf08f15
The10thHunter <mehtaprashant@icloud.com>
2024-12-30 09:09:02 -0800

    'Feauture' mispell preventing successful query of df

--- model.py
+++ model.py
@@ -4,11 +4,13 @@ import pandas as pd
 from sklearn.ensemble import GradientBoostingRegressor
 import random
 
+RAND = 99
+
 def basicGBRTrain(filtered_df): #n_est exists as a test var, it will be deployed via range() for testing
     #DEFINE CONSTS
     N_EST = 50
     LEARN_RATE = 1.25
-    RAND = 99
+    #RAND = 99
     #SETUP: 
     x = filtered_df[[a for a in filtered_df.columns if "feature" in a]]
     y = filtered_df["target"] #Main Target
@@ -24,10 +26,11 @@ def basicGBRTrain(filtered_df): #n_est exists as a test var, it will be deployed
 if __name__ == "__main__":
     df = pd.read_parquet("../v5.0/train.parquet")
     #Sample feature columns from lst 
-    features = df[[a for a in df.columns if "feauture" in a]].columns
+    features = df[[a for a in df.columns if "feature" in a]].columns
     for n in range(500, 2001, 500): 
-        random.seed(99)
+        random.seed(RAND)
         random_cols = random.sample(features, n)
         score = basicGBRTrain(df[[a for a in df.columns if a in random_cols or a == "target"]])
 
         print(f"Scored: {round(score, 4)} where random features = {n}")
+        """
